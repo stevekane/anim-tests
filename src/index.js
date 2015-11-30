bb01ac8a74aaa33732d2b59a75c9247aaae3e10b
Steve Kane <kanesteven@gmail.com>
2015-11-30 13:48:52 -0600

    throttled timestep to max of a frame

--- src/index.js
+++ src/index.js
@@ -6,29 +6,33 @@ import assets from './test-scene-1'
 const stage = document.createElement('div')
 const FPS = 24
 const FRAME_COUNT = 400
+const app = {
+  paused: false,
+  frame: 0
+}
 
 function makeUpdate () {
-  let then = performance.now()
-  let now = performance.now()
+  let then = 0
+  let now = 0
   let dT = now - then
   let elapsed = 0
-  let frame = 0
-  let paused = false
 
   return function update () {
     then = now
     now = performance.now()
-    dT = now - then    
-    paused = document.hidden || now - then  > 1000 / FPS
+    dT = now - then > 1000 / FPS ? 0 : now - then
+    app.paused = app.paused || document.hidden
 
-    if (!paused) {
+    pauseBtn.innerText = app.paused ? 'Play' : 'Pause'
+    stage.style.backgroundColor = app.paused ? 'gray' : 'white'
+    if (!app.paused) {
       elapsed = elapsed > FPS * FRAME_COUNT ? 0 : elapsed + dT
-      frame = elapsed / FPS
+      app.frame = elapsed / FPS
     }
     for (let asset of assets) {
-      asset.active = lerp(asset.keyframes.active, frame)
+      asset.active = lerp(asset.keyframes.active, app.frame)
     }
-    r.render(assets, frame)
+    r.render(assets, app.frame)
     requestAnimationFrame(update)
   }
 }
@@ -39,5 +43,7 @@ const pauseBtn = document.createElement('button')
 window.r = r
 stage.style.width = '800px'
 stage.style.height = '600px'
+document.body.appendChild(pauseBtn)
 document.body.appendChild(stage)
+pauseBtn.addEventListener('click', () => app.paused = !app.paused)
 requestAnimationFrame(makeUpdate())
